6ce0502d7f5ec00ca43da25f40439ecee25f10b4
H Root <hroot@pop-os.localdomain>
2022-04-25 15:52:22 +0300

    added react-prime packeges

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Dialog } from "primereact/dialog";
+import { Button } from "primereact/button";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [state, setState] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Dialog visible={state} onHide={() => setState(false)}></Dialog>
+      <Button />
     </div>
   );
-}
+};
 
 export default App;
